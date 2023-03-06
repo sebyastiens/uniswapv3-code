3f6e43f9115e2988161b529d918a2f8d11f1f685
0xPicsou21 <50265615+Heaven904@users.noreply.github.com>
2023-03-06 11:19:31 +0100

    Update UniswapV3Pool.sol

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.14;
 
-import "prb-math/PRBMath.sol";
+import "prb-math/Core.sol";
 
 import "./interfaces/IERC20.sol";
 import "./interfaces/IUniswapV3FlashCallback.sol";
