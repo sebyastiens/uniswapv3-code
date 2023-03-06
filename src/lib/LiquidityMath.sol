cb861c74bab0b761a15a4e16e8ae56f5282e1b86
0xPicsou21 <50265615+Heaven904@users.noreply.github.com>
2023-03-06 11:12:01 +0100

    Update LiquidityMath.sol
    https://github.com/PaulRBerg/prb-math/releases/tag/v3.0.0

--- src/lib/LiquidityMath.sol
+++ src/lib/LiquidityMath.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.14;
 
-import "prb-math/PRBMath.sol";
+import "prb-math/Core.sol";
 import "./FixedPoint96.sol";
 
 library LiquidityMath {
