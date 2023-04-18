fe2b8902eb1405994cf5ee1b2d173bed75964400
sebyastiens <sebyastiens@gmail.com>
2023-04-18 12:52:03 +0200

    Update SwapMath.sol

--- src/lib/SwapMath.sol
+++ src/lib/SwapMath.sol
@@ -8,52 +8,87 @@ library SwapMath {
         uint160 sqrtPriceCurrentX96,
         uint160 sqrtPriceTargetX96,
         uint128 liquidity,
-        uint256 amountRemaining
+        uint256 amountRemaining,
+        uint24 fee
     )
         internal
         pure
         returns (
             uint160 sqrtPriceNextX96,
             uint256 amountIn,
-            uint256 amountOut
+            uint256 amountOut,
+            uint256 feeAmount
         )
     {
         bool zeroForOne = sqrtPriceCurrentX96 >= sqrtPriceTargetX96;
+        uint256 amountRemainingLessFee = PRBMath.mulDiv(
+            amountRemaining,
+            1e6 - fee,
+            1e6
+        );
 
         amountIn = zeroForOne
             ? Math.calcAmount0Delta(
                 sqrtPriceCurrentX96,
                 sqrtPriceTargetX96,
-                liquidity
+                liquidity,
+                true
             )
             : Math.calcAmount1Delta(
                 sqrtPriceCurrentX96,
                 sqrtPriceTargetX96,
-                liquidity
+                liquidity,
+                true
             );
 
-        if (amountRemaining >= amountIn) sqrtPriceNextX96 = sqrtPriceTargetX96;
+        if (amountRemainingLessFee >= amountIn)
+            sqrtPriceNextX96 = sqrtPriceTargetX96;
         else
             sqrtPriceNextX96 = Math.getNextSqrtPriceFromInput(
                 sqrtPriceCurrentX96,
                 liquidity,
-                amountRemaining,
+                amountRemainingLessFee,
                 zeroForOne
             );
 
-        amountIn = Math.calcAmount0Delta(
-            sqrtPriceCurrentX96,
-            sqrtPriceNextX96,
-            liquidity
-        );
-        amountOut = Math.calcAmount1Delta(
-            sqrtPriceCurrentX96,
-            sqrtPriceNextX96,
-            liquidity
-        );
+        bool max = sqrtPriceNextX96 == sqrtPriceTargetX96;
 
-        if (!zeroForOne) {
-            (amountIn, amountOut) = (amountOut, amountIn);
+        if (zeroForOne) {
+            amountIn = max
+                ? amountIn
+                : Math.calcAmount0Delta(
+                    sqrtPriceCurrentX96,
+                    sqrtPriceNextX96,
+                    liquidity,
+                    true
+                );
+            amountOut = Math.calcAmount1Delta(
+                sqrtPriceCurrentX96,
+                sqrtPriceNextX96,
+                liquidity,
+                false
+            );
+        } else {
+            amountIn = max
+                ? amountIn
+                : Math.calcAmount1Delta(
+                    sqrtPriceCurrentX96,
+                    sqrtPriceNextX96,
+                    liquidity,
+                    true
+                );
+            amountOut = Math.calcAmount0Delta(
+                sqrtPriceCurrentX96,
+                sqrtPriceNextX96,
+                liquidity,
+                false
+            );
+        }
+
+        if (!max) {
+            feeAmount = amountRemaining - amountIn;
+        } else {
+            feeAmount = Math.mulDivRoundingUp(amountIn, fee, 1e6 - fee);
         }
     }
 }
